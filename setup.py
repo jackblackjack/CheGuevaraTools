03bb59d5c1e0d90b65ebc29165e0850605627335
mmenchu <mmenchu@gmail.com>
2014-04-04 07:14:39 -0400

    Making project a package

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='CheGuevaraTools',
+      version='1.0',
+      description='Multithreaded scraper with support for HTTP proxies',
+      author=' ',
+      author_email='gward@python.net',
+      url='https://github.com/mmenchu/CheGuevaraTools',
+      packages=['colorama', 'BeautifulSoup', 'html2text'],
+     )
